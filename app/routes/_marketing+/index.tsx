7310ec60d0f4a74842fbcac0f91c2d3a41a94bc4
Rohald van Merode <rohald_89@live.nl>
2024-10-03 15:01:12 +0200

    added skeleton for movie grid

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -1,9 +1,6 @@
-import {
-	json,
-	type LoaderFunctionArgs,
-	type MetaFunction,
-} from '@remix-run/node'
-import { Form, Link, useLoaderData } from '@remix-run/react'
+import { type LoaderFunctionArgs, type MetaFunction } from '@remix-run/node'
+import { defer, Await, Form, Link, useLoaderData } from '@remix-run/react'
+import { Suspense } from 'react'
 import { Button } from '#app/components/ui/button'
 import { cn } from '#app/utils/misc.tsx'
 import { getTopRatedMovies, type Movie } from '#app/utils/tmdb.server.ts'
@@ -14,8 +11,11 @@ export async function loader({ request }: LoaderFunctionArgs) {
 	const url = new URL(request.url)
 	const page = url.searchParams.get('page') || '1'
 	const pageNumber = parseInt(page, 10)
-	const { results, total_pages } = await getTopRatedMovies(pageNumber)
-	return json({ movies: { results, total_pages }, currentPage: pageNumber })
+	const movies = getTopRatedMovies(pageNumber).then(async (result) => {
+		await new Promise((resolve) => setTimeout(resolve, 1000))
+		return result
+	})
+	return defer({ movies, currentPage: pageNumber })
 }
 
 export default function Index() {
@@ -23,83 +23,129 @@ export default function Index() {
 	return (
 		<main className="font-poppins container mb-20 flex flex-col items-center justify-center gap-6">
 			<h1 className="text-h1">Top Rated Movies</h1>
-			<div className="mt-8 flex justify-center gap-4">
-				<Form>
-					<Button
-						type="submit"
-						name="page"
-						value={currentPage - 1}
-						disabled={currentPage <= 1}
-					>
-						Previous
-					</Button>
-				</Form>
-				<span className="flex items-center">
-					Page {currentPage} of {movies.total_pages}
-				</span>
-				<Form>
-					<Button
-						type="submit"
-						name="page"
-						value={currentPage + 1}
-						disabled={currentPage >= movies.total_pages}
-					>
-						Next
-					</Button>
-				</Form>
-			</div>
-			{movies.results.length ? (
-				<>
-					<ul
-						className={cn(
-							'grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5',
-						)}
-					>
-						{(movies.results as Movie[]).map((movie) => (
-							<li key={movie.id} className="w-full">
-								<Link
-									to={`/movies/${movie.id}`}
-									className="flex h-full flex-col items-center justify-between rounded-lg bg-muted p-3"
-								>
-									<img
-										src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
-										alt={movie.title}
-										className="w-full object-cover"
-									/>
-									<h2 className="text-small mt-2 line-clamp-2 text-center font-bold">
-										{movie.title}
-									</h2>
-								</Link>
-							</li>
-						))}
-					</ul>
-					<div className="mt-8 flex justify-center gap-4">
-						<Form>
-							<Button
-								type="submit"
-								name="page"
-								value={currentPage - 1}
-								disabled={currentPage <= 1}
+
+			<Suspense fallback={<MovieSkeletonGrid />}>
+				<Await resolve={movies}>
+					{(movies) => (
+						<>
+							<div className="flex justify-center gap-4">
+								<Form>
+									<Button
+										type="submit"
+										name="page"
+										value={currentPage - 1}
+										disabled={currentPage <= 1}
+									>
+										Previous
+									</Button>
+								</Form>
+								<span className="flex items-center">
+									Page {currentPage} of {movies.total_pages}
+								</span>
+								<Form>
+									<Button
+										type="submit"
+										name="page"
+										value={currentPage + 1}
+										disabled={currentPage >= movies.total_pages}
+									>
+										Next
+									</Button>
+								</Form>
+							</div>
+							<ul
+								className={cn(
+									'grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5',
+								)}
 							>
-								Previous
-							</Button>
-						</Form>
-						<span className="flex items-center">
-							Page {currentPage} of {movies.total_pages}
-						</span>
-						<Form>
-							<Button
-								type="submit"
-								name="page"
-								value={currentPage + 1}
-								disabled={currentPage >= movies.total_pages}
-							>
-								Next
-							</Button>
-						</Form>
-					</div>
-				</>
-			) : null}
+								{(movies.results as Movie[]).map((movie) => (
+									<li key={movie.id} className="w-full">
+										<Link
+											to={`/movies/${movie.id}`}
+											className="flex h-full flex-col items-center justify-between rounded-lg bg-muted p-3"
+										>
+											<img
+												src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
+												alt={movie.title}
+												className="w-full object-cover"
+											/>
+											<h2 className="text-small mt-2 line-clamp-2 text-center font-bold">
+												{movie.title}
+											</h2>
+										</Link>
+									</li>
+								))}
+							</ul>
+
+							<Pagination
+								currentPage={currentPage}
+								totalPages={movies.total_pages}
+							/>
+						</>
+					)}
+				</Await>
+			</Suspense>
 		</main>
+	)
+}
+
+function Pagination({
+	currentPage,
+	totalPages,
+}: {
+	currentPage: number
+	totalPages: number
+}) {
+	return (
+		<div className="flex justify-center gap-4">
+			<Form>
+				<Button
+					type="submit"
+					name="page"
+					value={currentPage - 1}
+					disabled={currentPage <= 1}
+				>
+					Previous
+				</Button>
+			</Form>
+			<span className="flex items-center">
+				Page {currentPage} of {totalPages}
+			</span>
+			<Form>
+				<Button
+					type="submit"
+					name="page"
+					value={currentPage + 1}
+					disabled={currentPage >= totalPages}
+				>
+					Next
+				</Button>
+			</Form>
+		</div>
+	)
+}
+
+function MovieSkeleton() {
+	return (
+		<div className="flex h-full w-full animate-pulse flex-col items-center justify-between rounded-lg bg-muted p-3">
+			<div className="h-[185px] w-full rounded bg-gray-300"></div>
+			<div className="mt-2 h-4 w-3/4 rounded bg-gray-300"></div>
+		</div>
+	)
+}
+
+function MovieSkeletonGrid() {
+	return (
+		<ul
+			className={cn(
+				'grid w-full grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5',
+			)}
+		>
+			{Array.from({ length: 20 }).map((_, index) => (
+				<li key={index} className="w-full">
+					<MovieSkeleton />
+				</li>
+			))}
+		</ul>
 	)
 }
