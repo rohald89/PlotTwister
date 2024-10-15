de72cf078d4e884d3a154785faea99587dae68a3
Rohald van Merode <rohald_89@live.nl>
2024-10-16 01:16:50 +0200

    fixed config

--- eslint.config.js
+++ eslint.config.js
@@ -4,21 +4,4 @@ import { default as defaultConfig } from '@epic-web/config/eslint'
 export default [
 	...defaultConfig,
 	// add custom config objects here:
-	{
-		root: true,
-		parserOptions: {
-			tsconfigRootDir: __dirname,
-			project: ['./tsconfig.json'],
-		},
-		ignorePatterns: ['.eslintrc.js'],
-		overrides: [
-			{
-				files: ['*.ts', '*.tsx'],
-				extends: [
-					'plugin:@typescript-eslint/recommended',
-					'plugin:@typescript-eslint/recommended-requiring-type-checking',
-				],
-			},
-		],
-	},
 ]
