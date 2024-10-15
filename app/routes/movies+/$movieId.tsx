e19701e1ad14571ca4eeefa99d44d99bf6109a11
Rohald van Merode <rohald_89@live.nl>
2024-10-15 17:57:20 +0200

    added Tabs and Accordion to movie detail page

--- app/routes/movies+/$movieId.tsx
+++ app/routes/movies+/$movieId.tsx
@@ -2,8 +2,20 @@ import { json, type LoaderFunctionArgs } from '@remix-run/node'
 import { useLoaderData, useNavigate, type MetaFunction } from '@remix-run/react'
 import { useState, useRef } from 'react'
 import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from '#app/components/ui/accordion'
 import { Button } from '#app/components/ui/button.tsx'
 import { Icon } from '#app/components/ui/icon.tsx'
+import {
+	Tabs,
+	TabsContent,
+	TabsList,
+	TabsTrigger,
+} from '#app/components/ui/tabs.js'
 import { prisma } from '#app/utils/db.server.ts'
 import {
 	formatRuntime,
@@ -152,8 +164,37 @@ export default function MovieRoute() {
 							<p className="text-h6">PG13</p>
 						</div>
 						<div className="flex-1">
-							<h2 className="mb-4 text-h6">Storyline</h2>
-							<p>{movie.overview}</p>
+							<Tabs defaultValue="story" className="w-[400px]">
+								<TabsList>
+									<TabsTrigger value="story">Storyline</TabsTrigger>
+									<TabsTrigger value="alternate">Alternate Endings</TabsTrigger>
+									<TabsTrigger value="new">New</TabsTrigger>
+								</TabsList>
+								<TabsContent value="story">{movie.overview}</TabsContent>
+								<TabsContent value="alternate">
+									<div className="flex flex-col gap-4">
+										<Accordion type="single" collapsible>
+											{data.alternateEndings.map((ending) => (
+												<AccordionItem key={ending.id} value={ending.id}>
+													<AccordionTrigger>
+														<>
+															{ending.title}
+															<i>by {ending.author.username}</i>
+														</>
+													</AccordionTrigger>
+													<AccordionContent>{ending.content}</AccordionContent>
+												</AccordionItem>
+											))}
+										</Accordion>
+									</div>
+								</TabsContent>
+								<TabsContent value="new">
+									<AlternateEndingEditor
+										tmdbMovieId={Number(movie.id)}
+										movieTitle={movie.title}
+									/>
+								</TabsContent>
+							</Tabs>
 						</div>
 						<div className="w-1/4">
 							<h2 className="mb-4 text-h6">Cast</h2>
@@ -177,129 +218,10 @@ export default function MovieRoute() {
 						</div>
 					</div>
 				</div>
-				<div className="w-1/5">
-					<h2 className="text-h4">Alternate Endings</h2>
-					<div className="flex flex-col gap-4">
-						{data.alternateEndings.map((ending) => (
-							<div key={ending.id} className="my-4 rounded-lg border p-4">
-								<h3 className="text-h4">{ending.title}</h3>
-								<p className="mb-3 italic">by {ending.author.username}</p>
-							</div>
-						))}
-					</div>
-					<Button
-						onClick={() => {
-							setShowEditor(true)
-							setTimeout(
-								() => editorRef.current?.scrollIntoView({ behavior: 'smooth' }),
-								0,
-							)
-						}}
-					>
-						Create New Ending
-					</Button>
-					{showEditor && (
-						<div ref={editorRef} className="mt-8 w-full max-w-3xl">
-							<h2 className="mb-4 text-h3">
-								{data.alternateEndings.length === 0 ? 'Create' : 'Add'}{' '}
-								Alternate Ending
-							</h2>
-							<AlternateEndingEditor
-								tmdbMovieId={Number(movie.id)}
-								movieTitle={movie.title}
-							/>
-						</div>
-					)}
+				<div className="w-1/5 border-l-2 border-muted-foreground px-4">
+					<h2 className="text-h6">More like this</h2>
 				</div>
 			</div>
-			{/* <div className="container mb-48 mt-4 flex flex-col items-center justify-center">
-				<Button
-					variant="ghost"
-					onClick={() => navigate(-1)}
-					className="mb-4 self-start"
-				>
-					<Icon name="arrow-left" className="mr-2" />
-					Back
-				</Button>
-
-				<Spacer size="4xl" />
-
-				<div
-					className="cover flex flex-col rounded-3xl bg-muted bg-cover bg-center"
-					style={{
-						backgroundImage: `url(https://image.tmdb.org/t/p/w780${movie.backdrop_path})`,
-					}}
-				>
-					<div className="relative min-h-[400px] w-full rounded-3xl bg-backdrop-gradient-light dark:bg-backdrop-gradient-dark">
-						<div className="absolute -top-40 left-10">
-							<div className="relative flex items-center gap-6">
-								<img
-									className="rounded-sm"
-									src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
-									alt={movie.title}
-								></img>
-								<h1 className="text-h2">{movie.title}</h1>
-							</div>
-						</div>
-
-						<div className="flex min-h-96 gap-8 p-8 pl-16 pt-10">
-							<div className="mt-32 min-w-40 space-y-3">
-								<div>
-									<h2 className="text-sm font-bold">Release Date</h2>
-									<p className="text-sm">{movie.release_date}</p>
-								</div>
-								<div>
-									<h2 className="text-sm font-bold">Score</h2>
-									<p className="text-sm">{movie.vote_average}/10</p>
-								</div>
-								<div>
-									<h2 className="text-sm font-bold">Runtime</h2>
-									<p className="text-sm">{movie.runtime} minutes</p>
-								</div>
-							</div>
-							<div className="">{movie.overview}</div>
-						</div>
-					</div>
-				</div>
-
-				<div className="w-full max-w-3xl">
-					<div className="flex items-center justify-between">
-						<h2 className="text-h3">Alternate Endings</h2>
-						<Button
-							onClick={() => {
-								setShowEditor(true)
-								setTimeout(
-									() =>
-										editorRef.current?.scrollIntoView({ behavior: 'smooth' }),
-									0,
-								)
-							}}
-						>
-							Create New Ending
-						</Button>
-					</div>
-					{data.alternateEndings.map((ending) => (
-						<div key={ending.id} className="my-4 rounded-lg border p-4">
-							<h3 className="text-h4">{ending.title}</h3>
-							<p className="mb-3 italic">by {ending.author.username}</p>
-							<p>{ending.content}</p>
-						</div>
-					))}
-				</div>
-
-				{showEditor && (
-					<div ref={editorRef} className="mt-8 w-full max-w-3xl">
-						<h2 className="mb-4 text-h3">
-							{data.alternateEndings.length === 0 ? 'Create' : 'Add'} Alternate
-							Ending
-						</h2>
-						<AlternateEndingEditor
-							tmdbMovieId={Number(movie.id)}
-							movieTitle={movie.title}
-						/>
-					</div>
-				)}
-			</div>  */}
 		</>
 	)
 }
--- app/utils/extended-theme.ts
+++ app/utils/extended-theme.ts
@@ -99,8 +99,26 @@ export const extendedTheme = {
 			'0%,70%,100%': { opacity: '1' },
 			'20%,50%': { opacity: '0' },
 		},
+        'accordion-down': {
+            from: {
+                height: '0'
+            },
+            to: {
+                height: 'var(--radix-accordion-content-height)'
+            }
+        },
+        'accordion-up': {
+            from: {
+                height: 'var(--radix-accordion-content-height)'
+            },
+            to: {
+                height: '0'
+            }
+        }
 	},
 	animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
 		'caret-blink': 'caret-blink 1.25s ease-out infinite',
 	},
 } satisfies Config['theme']
