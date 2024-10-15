7777d0b0a200b927c5f93a1cea1cbc388c20435b
Rohald van Merode <rohald_89@live.nl>
2024-10-15 23:37:57 +0200

    Styling and readability improvements

--- app/routes/movies+/$movieId.tsx
+++ app/routes/movies+/$movieId.tsx
@@ -5,6 +5,7 @@ import {
 	type MetaFunction,
 	useLocation,
 	useFetcher,
+	Link,
 } from '@remix-run/react'
 import { useState, useRef } from 'react'
 import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
@@ -24,17 +25,35 @@ import {
 } from '#app/components/ui/tabs.js'
 import { prisma } from '#app/utils/db.server.ts'
 import {
+	cn,
 	formatRuntime,
 	formatVoteCount,
 	getReleaseYear,
 } from '#app/utils/misc.js'
-import { getMovie } from '#app/utils/tmdb.server.ts'
+import { getMovie, MovieListItem } from '#app/utils/tmdb.server.ts'
 import { AlternateEndingEditor } from './__ending-editor'
 import { useToast } from '#app/components/toaster'
 import { requireUserId } from '#app/utils/auth.server.ts'
 import { Toast } from '#app/utils/toast.server.js'
 import { MovieTrailerDialog } from '#app/components/movietrailer-dialog'
 import { WatchProvidersDialog } from '#app/components/watch-provider-dialog'
+import { ScrollArea } from '#app/components/ui/scroll-area.js'
+
+function useLike(initialLiked: boolean) {
+	const likeFetcher = useFetcher()
+	const liked = likeFetcher.formData
+		? likeFetcher.formData.get('liked') === 'true'
+		: initialLiked
+
+	const handleLike = (movieId: number) => {
+		likeFetcher.submit(null, {
+			method: 'post',
+			action: `/api/movies/${movieId}/like`,
+		})
+	}
+
+	return { liked, handleLike }
+}
 
 export async function loader({ params, request }: LoaderFunctionArgs) {
 	const userId = await requireUserId(request)
@@ -71,7 +90,8 @@ export { action } from './__ending-editor.server'
 export default function MovieRoute() {
 	const { movie, isLiked, alternateEndings, trailerKey } =
 		useLoaderData<typeof loader>()
-	const likeFetcher = useFetcher()
+
+	const { liked, handleLike } = useLike(isLiked)
 	const navigate = useNavigate()
 	const location = useLocation()
 	const [toast, setToast] = useState<Toast | null>(null)
@@ -79,17 +99,7 @@ export default function MovieRoute() {
 	const [isTrailerDialogOpen, setIsTrailerDialogOpen] = useState(false)
 	const [isWatchProvidersDialogOpen, setIsWatchProvidersDialogOpen] =
 		useState(false)
-
-	const handleLike = () => {
-		likeFetcher.submit(null, {
-			method: 'post',
-			action: `/api/movies/${movie.id}/like`,
-		})
-	}
-
-	const liked = likeFetcher.formData
-		? likeFetcher.formData.get('liked') === 'true'
-		: isLiked
+	const [activeTab, setActiveTab] = useState('story')
 
 	const [showEditor, setShowEditor] = useState(false)
 	const editorRef = useRef<HTMLDivElement>(null)
@@ -151,7 +161,7 @@ export default function MovieRoute() {
 							<Button
 								size="xl"
 								variant="outline"
-								className="rounded-full bg-muted px-12 py-8 text-xl"
+								className="rounded-full border-none bg-transparent px-12 py-8 text-xl ring-2 ring-primary ring-offset-0"
 								onClick={() => setIsTrailerDialogOpen(true)}
 							>
 								<Icon name="play" className="mr-6" />
@@ -185,7 +195,7 @@ export default function MovieRoute() {
 							<div className="flex gap-2">
 								<Button
 									size="xl"
-									variant="outline"
+									variant="secondary"
 									className="rounded-full px-12 py-8 text-xl"
 									onClick={() => setIsWatchProvidersDialogOpen(true)}
 								>
@@ -198,7 +208,7 @@ export default function MovieRoute() {
 									className={`rounded-full px-6 py-8 text-xl outline-none ring-0 focus-within:ring-0 focus-visible:ring-0 ${
 										liked ? 'border-red-500 text-red-500' : ''
 									}`}
-									onClick={handleLike}
+									onClick={() => handleLike(movie.id)}
 								>
 									<Icon name={liked ? 'heart-filled' : 'heart'} />
 								</Button>
@@ -222,13 +232,21 @@ export default function MovieRoute() {
 					</div>
 
 					<div className="flex gap-10">
-						<div className="w-32 uppercase">
-							<p className="text-h6">{getReleaseYear(movie.release_date)}</p>
-							<p className="text-h6">{formatRuntime(movie.runtime)}</p>
-							<p className="text-h6">PG13</p>
+						<div className="w-32 space-y-2 uppercase">
+							<p className="text-h4 font-thin">
+								{getReleaseYear(movie.release_date)}
+							</p>
+							<p className="text-h4 font-thin">
+								{formatRuntime(movie.runtime)}
+							</p>
+							<p className="text-h4 font-thin">PG13</p>
 						</div>
 						<div className="flex-1">
-							<Tabs defaultValue="story" className="w-[400px]">
+							<Tabs
+								defaultValue="story"
+								value={activeTab}
+								onValueChange={setActiveTab}
+							>
 								<TabsList>
 									<TabsTrigger value="story">Storyline</TabsTrigger>
 									<TabsTrigger value="alternate">Alternate Endings</TabsTrigger>
@@ -236,21 +254,41 @@ export default function MovieRoute() {
 								</TabsList>
 								<TabsContent value="story">{movie.overview}</TabsContent>
 								<TabsContent value="alternate">
-									<div className="flex flex-col gap-4">
-										<Accordion type="single" collapsible>
-											{alternateEndings.map((ending) => (
-												<AccordionItem key={ending.id} value={ending.id}>
-													<AccordionTrigger>
-														<>
-															{ending.title}
-															<i>by {ending.author.username}</i>
-														</>
-													</AccordionTrigger>
-													<AccordionContent>{ending.content}</AccordionContent>
-												</AccordionItem>
-											))}
-										</Accordion>
-									</div>
+									{alternateEndings.length > 0 ? (
+										<div className="flex flex-col gap-4">
+											<Accordion type="single" collapsible>
+												{alternateEndings.map((ending) => (
+													<AccordionItem key={ending.id} value={ending.id}>
+														<AccordionTrigger>
+															<>
+																{ending.title}
+																<i>by {ending.author.username}</i>
+															</>
+														</AccordionTrigger>
+														<AccordionContent>
+															{ending.content}
+														</AccordionContent>
+													</AccordionItem>
+												))}
+											</Accordion>
+										</div>
+									) : (
+										<div className="flex flex-col items-center justify-center py-8">
+											<p className="text-center text-muted-foreground">
+												No alternate endings have been created yet.
+											</p>
+											<p className="text-center text-muted-foreground">
+												Be the first to write one!
+											</p>
+											<Button
+												onClick={() => setActiveTab('new')}
+												variant="secondary"
+												className="mt-8"
+											>
+												Create Alternate Ending
+											</Button>
+										</div>
+									)}
 								</TabsContent>
 								<TabsContent value="new">
 									<AlternateEndingEditor
@@ -263,61 +301,33 @@ export default function MovieRoute() {
 						<div className="w-1/4">
 							<h2 className="mb-4 text-h6">Cast</h2>
 							<div className="flex flex-col gap-4">
-								{movie.credits?.cast.slice(0, 5).map((cast) => (
-									<div key={cast.id} className="flex items-center gap-4">
-										<img
-											className="aspect-square h-16 w-16 rounded-full object-cover"
-											src={`https://image.tmdb.org/t/p/w185${cast.profile_path}`}
-											alt={cast.name}
-										></img>
-										<div>
-											<p>{cast.name}</p>
-											<p className="text-body-xs opacity-60">
-												{cast.character}
-											</p>
-										</div>
-									</div>
-								))}
+								<ScrollArea className="h-64">
+									{movie.credits?.cast.map((cast) => (
+										<Link
+											to={`/people/${cast.id}`}
+											key={cast.id}
+											className="flex items-center gap-4 [&:not(:first-of-type)]:mt-4"
+										>
+											<img
+												className="aspect-square h-16 w-16 rounded-full object-cover"
+												src={`https://image.tmdb.org/t/p/w92${cast.profile_path}`}
+												alt={cast.name}
+											></img>
+											<div>
+												<p>{cast.name}</p>
+												<p className="text-body-xs opacity-60">
+													{cast.character}
+												</p>
+											</div>
+										</Link>
+									))}
+								</ScrollArea>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div className="w-[27%] border-l-2 border-muted-foreground px-4">
-					<h2 className="mb-10 text-h6">More like this</h2>
-					<div className="flex flex-col gap-10 px-8">
-						{movie.recommendations?.results
-							.splice(0, 3)
-							.map((recommendation) => (
-								<div
-									className="flex items-center gap-6"
-									key={recommendation.id}
-								>
-									<img
-										className="object-cover"
-										src={`https://image.tmdb.org/t/p/w92${recommendation.poster_path}`}
-										alt={recommendation.title}
-									></img>
-									<div className="flex flex-col gap-5">
-										<p>{recommendation.title}</p>
-										<div className="flex items-center gap-3">
-											<MovieRating
-												size="sm"
-												vote_average={recommendation.vote_average}
-											/>
-											<div>
-												<p className="text-h6">
-													{getReleaseYear(recommendation.release_date)}
-												</p>
-												<p className="text-h6">
-													{formatVoteCount(recommendation.vote_count)}
-												</p>
-											</div>
-										</div>
-									</div>
-								</div>
-							))}
-					</div>
-				</div>
+
+				<MovieRecommendations recommendations={movie.recommendations.results} />
 			</div>
 			{trailerKey && (
 				<MovieTrailerDialog
@@ -335,6 +345,50 @@ export default function MovieRoute() {
 				country="NL"
 			/>
 		</>
+	)
+}
+
+function MovieRecommendations({
+	recommendations,
+}: {
+	recommendations: MovieListItem[]
+}) {
+	return (
+		<div className="w-[27%] border-l-2 border-muted-foreground px-4">
+			<h2 className="mb-10 text-h6">More like this</h2>
+			<ScrollArea className="h-[500px] px-8">
+				{recommendations.map((recommendation) => (
+					<Link
+						to={`/movies/${recommendation.id}`}
+						className="flex items-center gap-6 [&:not(:first-of-type)]:mt-8"
+						key={recommendation.id}
+					>
+						<img
+							className="object-cover"
+							src={`https://image.tmdb.org/t/p/w92${recommendation.poster_path}`}
+							alt={recommendation.title}
+						></img>
+						<div className="flex flex-col gap-5">
+							<p>{recommendation.title}</p>
+							<div className="flex items-center gap-3">
+								<MovieRating
+									size="sm"
+									vote_average={recommendation.vote_average}
+								/>
+								<div>
+									<p className="text-h6">
+										{getReleaseYear(recommendation.release_date)}
+									</p>
+									<p className="text-h6">
+										{formatVoteCount(recommendation.vote_count)}
+									</p>
+								</div>
+							</div>
+						</div>
+					</Link>
+				))}
+			</ScrollArea>
+		</div>
 	)
 }
 
@@ -361,7 +415,12 @@ function MovieRating({
 					className="stroke-primary"
 				/>
 			</svg>
-			<div className="absolute inset-0 flex items-center justify-center text-lg font-bold">
+			<div
+				className={cn(
+					'absolute inset-0 flex items-center justify-center font-bold',
+					size === 'sm' ? 'text-h6' : size === 'md' ? 'text-h5' : 'text-h4',
+				)}
+			>
 				{vote_average.toFixed(1)}
 			</div>
 		</div>
